cabd577ee3ce177d9b1aa89c196daf3bcd75134e
gundeepsingh <gundeep.upenn@gmail.com>
2022-10-30 20:48:35 +0100

    Added 404 response for non understood requests

--- rustdevelopement/WebServer/webserver/src/main.rs
+++ rustdevelopement/WebServer/webserver/src/main.rs
@@ -1,13 +1,11 @@
-use std::net::TcpListener;
-use std::io::prelude::*;
-use std::net::TcpStream;
 use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 
-fn main()
-{
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming()
-    {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
         //println!("Conn Succeeded");
@@ -15,20 +13,34 @@ fn main()
 }
 // I guess in Rust by default all is immutable?
 // This is mutable here because internal state of stream can keep changing.
-fn handle_connection(mut stream: TcpStream)
-{
-    let mut buffer = [0;1024];
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    // handle response
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(get) {
+        // handle response
+        let contents = fs::read_to_string("hello.html").unwrap();
 
-    let response = format!("HTTP/1.1 200 OK \r\n Content-Length: {}\r\n\r\n{}", contents.len() , contents);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        let response = format!(
+            "HTTP/1.1 200 OK \r\n Content-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
 
-    //let response = "HTTP/1.1 200 OK \r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+        //let response = "HTTP/1.1 200 OK \r\n\r\n";
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    } else {
+        //some other request return 404
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status_line, contents.len(), contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
