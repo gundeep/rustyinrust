3e7c5618edf3465b6dfff887eab67997017d7f50
gundeepsingh <gundeep.upenn@gmail.com>
2022-11-01 07:16:24 +0100

    WIP use threadpool to make server MT, added git ignore

--- rustdevelopement/WebServer/webserver/src/bin/main.rs
+++ rustdevelopement/WebServer/webserver/src/bin/main.rs
@@ -3,11 +3,19 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+use webserver::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    //create a thread pool
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        //execute the thread pool
+        pool.execute(|| {
+            handle_connection(stream);
+        });
         //println!("Conn Succeeded");
     }
 }
---
+++ rustdevelopement/WebServer/webserver/src/lib.rs
@@ -0,0 +1,17 @@
+// Create ThreadPool struct
+pub struct ThreadPool;
+// impl block
+impl ThreadPool {
+    // new method
+    pub fn new(size: usize) -> ThreadPool {
+        // create a new ThreadPool
+        ThreadPool
+    }
+    // execute method
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
+
