7e89f0d7e2adbd1a737bb19e7a983a4b76c2dd98
gundeep <gundeep.upenn@gmail.com>
2023-03-31 21:57:41 +0200

    syntax

--- rustdevelopement/WebServer/webserver/src/lib.rs
+++ rustdevelopement/WebServer/webserver/src/lib.rs
@@ -3,12 +3,12 @@ pub struct ThreadPool;
 // impl block
 impl ThreadPool {
     // new method
-    pub fn new(size: usize) -> ThreadPool {
+    pub fn new(_size: usize) -> ThreadPool {
         // create a new ThreadPool
         ThreadPool
     }
     // execute method
-    pub fn execute<F>(&self, f: F)
+    pub fn execute<F>(&self, _f: F)
     where
         F: FnOnce() + Send + 'static,
     {
